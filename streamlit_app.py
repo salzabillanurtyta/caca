45ad09e060f3b843a2fd67e802cf759b759f2594
salzabillanurtyta <salzabillanurtytasalamah@gmail.com>
2025-05-22 11:33:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,12 @@ st.subheader("cacuca")
 st.write(
     "coba liat coba"
 )
+
+#with col1 :
+st.harder ("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2) ==0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
